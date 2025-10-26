6d59008392bedcb7268d6f23e1a3756150224004
Sergio Paya Borrull <YOUR_GITHUB_PRIMARY_EMAIL>
2025-10-26 13:54:06 +0000

    Add all 295 files - L.I.F.E Platform complete

---
+++ SUPER_SIMPLE_COMMIT.bat
@@ -0,0 +1,8 @@
+@echo off
+echo SUPER SIMPLE: Add and commit everything right now
+git add .
+git add -A
+git commit -m "Add all 295 files - L.I.F.E Platform complete"
+git push origin main
+echo Done! Check if it worked.
+pause
